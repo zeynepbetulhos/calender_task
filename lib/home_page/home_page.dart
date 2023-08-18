f26b5055f0a33bd5d1e86c8f2010e51a0024e162
Zeynep Betul Hos <zeynepbtl003@gmail.com>
2023-08-18 19:03:06 +0300

    first commit

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -8,7 +8,6 @@ import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
